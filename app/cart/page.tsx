87df80a1272d087264ebff7b9d96d742042ca48b
sujal shestha <sujal84071@gmail.com>
2025-09-29 13:57:50 +0545

    created a shooping card list

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,3 +1,30 @@
+"use client";
+import { useState } from "react";
+import { products } from "../product-data";
+import Link from "next/link";
 export default function CartPage() {
-    return <>Product Cart</>
+    const [cartIds, setCartIds] = useState(["123", "345"]);
+
+    const cartProducts = cartIds
+        .map((id) => products.find((p) => p.id === id))
+        .filter((p): p is NonNullable<typeof p> => p !== undefined);
+    console.log(cartProducts)
+
+    return (
+        <div>
+            <h1>Shooping cart</h1>
+            {cartProducts.length === 0 ? (
+                <p>Your cart is empty!</p>
+            ) : (
+                cartProducts.map((product) => (
+                    <Link key={product.id} href={"/products/" + product.id}>
+                        <br />
+                        <h3>{product.name}</h3>
+                        <p>${product.price}</p>
+                        <br />
+                    </Link>
+                )))
+            }
+        </div>
+    );
 }
--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,6 +1,23 @@
+import { products } from "@/app/product-data";
 
-import { products } from "@/app/product-data"
-export default function ProductDetailsPage({ params }: { params: { id: string } }) {
-    const product = products.find(p => p.id === params.id);
-    return <h1>{product!.name}</h1>
+export default async function ProductDetailsPage({
+    params,
+}: {
+    params: Promise<{ id: string }>;
+}) {
+    const { id } = await params;
+
+    const product = products.find((p) => p.id === id);
+
+    if (!product) {
+        return <h1>Product not found</h1>;
+    }
+
+    return (
+        <div>
+            <h1>Product Name: {product.name}</h1>
+            <p>Price: ${product.price}</p>
+            <p>Description: {product.description}</p>
+        </div>
+    );
 }
